c8187d4217b4fbbeb7ee6284fc5737eb785fb5db
Kahfi0911 <muhammadkahfi0911@gmail.com>
2023-01-07 22:54:25 +0800

    Developer Android

---
+++ pertemuan 1/AddLog_011/app/src/main/java/ac/id/poliban/mi/kahfi/addlog_011/MainActivity.java
@@ -0,0 +1,16 @@
+package ac.id.poliban.mi.kahfi.addlog_011;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
